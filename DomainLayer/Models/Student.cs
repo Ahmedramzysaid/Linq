9c4f3036f931b90dfe71a7280e681d98102918a8
Ramzy <ramzyis258@gmail.com>
2026-04-16 00:37:12 +0200

    Domain Layer

---
+++ DomainLayer/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Major { get; set; }
+    }
+}
